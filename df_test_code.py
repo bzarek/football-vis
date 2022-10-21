44cc03cd5b68aea7060f674eb08d4ad3e770bc4f
bzarek <bzarek@gmail.com>
2022-10-20 20:39:57 -0400

    table format test

--- df_test_code.py
+++ df_test_code.py
@@ -1,16 +1,18 @@
 from read_sheets import read_sheets
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
+df1 = read_sheets()
+json_data = df1.to_json(orient="columns")
+df2 = pd.read_json(json_data, orient="columns")
 
-df = read_sheets()
-df_reduced = df[["Name", "Week", "Correct?"]].copy()
-df_reduced.rename(columns={"Correct?":"Correct Picks"}, inplace=True)
-df_organized = df_reduced.groupby(["Name","Week"], as_index=False).sum(numeric_only=True).sort_values(["Name","Week"]).copy()
-df_organized["Cumulative Wins"] = df_organized.groupby("Name")["Correct Picks"].transform(pd.Series.cumsum)
+df_table = df2[["Name","Correct?", "Incorrect?", "Push?", "Profit"]].copy()
+df_table = df_table.groupby("Name", as_index=False).sum(numeric_only=True).sort_values(by=["Correct?", "Profit"], ascending=False)
 
-#print(df)
-#print(df_reduced)
-print(df_organized)
+#format profit and record
+df_table["Profit"] = df_table["Profit"].apply(lambda x : f"${x:.2f}" if x>0 else f"-${-x:.2f}")
+df_table["Record"] = df_table.apply(lambda x: f"{x['Correct?']}-{x['Incorrect?']}-{x['Push?']}", axis="columns")
+df_table.drop(columns=["Correct?", "Incorrect?", "Push?"], inplace=True)
 
-#px.bar(df_processed, y="Correct?", labels={"Name", "Correct Picks"}).show()
+print(df_table[["Name", "Record", "Profit"]])
